78ab3b6d6d74ddb5fc04669b78130c153923507d
Harsh-rao18 <harshrao64644@gmail.com>
2025-10-25 14:06:49 +0530

    Learning Spring Data Jpa and Spring Data REST

---
+++ 02-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/harsh/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.harsh.cruddemo.dao;
+
+import com.harsh.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    // that's it .... no need to write any code
+}
---
+++ 02-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/harsh/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.harsh.cruddemo.service;
+
+import com.harsh.cruddemo.dao.EmployeeRepository;
+import com.harsh.cruddemo.entity.Employee;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+    EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee theEmployee = null;
+        if (result.isPresent()){
+            theEmployee = result.get();
+        } else {
+            throw  new RuntimeException("Employee not Found");
+        }
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+
+}
